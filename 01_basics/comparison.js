0f2aa38e33626794d136f90f11c489d7ab827ca6
Pratik Raut <139999671+LearningPratik@users.noreply.github.com>
2024-11-28 05:08:56 +0000

    comparison and stack&heap data type

---
+++ 01_basics/comparison.js
@@ -0,0 +1,7 @@
+console.log(2 == 2);
+
+// JS will convert datatype and bring them both onto same values
+console.log("2" == 2);
+
+// for this do strict comparison --> check value and also it's datatype
+console.log("2" === 2);
--- 01_basics/dataTypes.js
+++ 01_basics/dataTypes.js
@@ -20,3 +20,53 @@ let isLoggedIn = true;
 // objects
 console.log(typeof null); // type of null is object
 console.log(typeof undefined); // type of undefined is undefined
+
+// Primitive data type --> call by value
+// It does not give you reference to the memory, if changes made, it does in it's copy
+
+// String, Number, Boolean, Null, Undefined, Symbol, BigInt, 
+
+// Non-primitive --> Reference type
+// allocates memory
+
+// arrays, objects, functions
+let arr = ["hello", "bye", "good"];
+
+let myObj = {
+
+    name: "Pratik",
+    age: "50"
+};
+
+const myfunc = function(){
+    console.log("hello");
+    
+};
+
+console.log(typeof myfunc);
+
+// See ECMA online resource for data types reference
+
+
+// all primitive type datatype goes to stack memory
+let myname = "Pratik";
+let myname2 = myname;
+
+myname2 = "Dhruv";
+
+console.log(myname);
+console.log(myname2);
+
+// all non-primitive type datatype goes to heap memory
+
+let user1 = {
+
+    email: "user@gmail.com",
+    upiid: "upi@no"
+
+}
+
+let user2 = user1;
+
+console.log(user1);
+console.log(user2);
